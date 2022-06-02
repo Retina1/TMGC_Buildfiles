34627da9c8514e2283888f1026910b13993a6822
Retina1 <84415148+Retina1@users.noreply.github.com>
2022-06-01 23:05:27 -0400

    a few talks, ch13 enemy placement cont.

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/TowerShieldPlus/TowerShieldPlus.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/TowerShieldPlus/TowerShieldPlus.s
@@ -17,7 +17,7 @@ mov r0, r4
 ldr r1, TowerShieldPlusID
 .short 0xf800
 cmp r0, #0
-beq GoBack
+beq End
 
 @set both attacker and defender attack to 50 percent
 mov r1, #0x5a
@@ -28,7 +28,21 @@ ldrsh r0, [r5, r1] @atk
 lsr r0,r0,#0x1
 strh r0, [r5,r1]
 
-GoBack:
+mov  r2,#0x02
+ldrb r1, [r7]
+and  r1, r2
+cmp  r1, r2
+bne  End//if bit 2 is set, battle has NOT started
+mov  r2, #0x5c
+ldrh r3, [r5,r2]//load defender def
+mov  r2, #0x5A//atk offset
+ldrh r1, [r4,r2]//load attacker atk
+add  r1,r3
+strh r1, [r4,r2]
+b    End
+//endofcode
+
+End:
 pop {r4-r7}
 pop {r0}
 bx r0

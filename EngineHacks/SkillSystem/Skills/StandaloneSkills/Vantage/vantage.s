31f0dc7bd0a7f0dee4de8c6c388ec6c400b14c9c
Retina1 <84415148+Retina1@users.noreply.github.com>
2022-07-08 23:33:09 -0400

    vantage working, slight cleanup, begin zeke writing

--- EngineHacks/SkillSystem/Skills/StandaloneSkills/Vantage/vantage.s
+++ EngineHacks/SkillSystem/Skills/StandaloneSkills/Vantage/vantage.s
@@ -7,7 +7,22 @@ ldr r4, =0x203a4ec @atr
 ldr r5, =0x203a56c @dfr
 mov r6, r0 @place to store attacker
 mov r7, r1 @place to store defender
-@check for Vantage, Vantage+ 
+@check for Vantage, Vantage+
+
+mov r0, #0xAF
+ldr r1, =#0x8083DA8
+mov r14, r1
+.short 0xF800
+cmp r0, #1 
+bne Continue
+ldrb r0, [r5, #0xB] @ deployment byte 
+lsr r0, #7 @ enemy bit only 
+cmp r0, #0 
+bne VantagePlus @ if enemy and flag on, always do vantage
+
+Continue:
+ldr r0, SkillTester
+ 
 ldr r0, SkillTester
 mov lr, r0
 mov r0, r5 @defender data
@@ -38,37 +53,12 @@ eor r5,r4
 eor r4,r5
 b Normal
 
-@if showing animation
-@ ldr     r0,=0x802b444    @pointer to the current round
-@ ldr     r0, [r0]          @current round pointer (usually 203a608)
-@ ldr     r3, [r0] @203aac0 + (8*round number)
 
-@ ldr     r2,[r3]    
-@ lsl     r1,r2,#0xD                @ 0802B42C 0351     
-@ lsr     r1,r1,#0xD                @ 0802B42E 0B49     
-@ mov     r0, #0x40
-@ lsl     r0, #8           @0x4100, attacker skill activated
-@ orr     r1, r0
-@ ldr     r0,=#0xFFF80000                @ 0802B434 4804     
-@ and     r0,r2                @ 0802B436 4010     
-@ orr     r0,r1                @ 0802B438 4308     
-@ str     r0,[r3]                @ 0802B43A 6018  
-
-@ ldrb  r0, VantageID
-@ strb  r0, [r3,#4] 
 
 VantagePlus:
 eor r4,r5
 eor r5,r4
 eor r4,r5
-mov r1, #0x66 @crit
-mov r0, #0
-strh r0, [r4,r1]
-@mov r1, #0x68 @crit avoid
-@mov r0, #0
-@strh r0, [r4,r1]
-mov r1, #0x6A @battle crit
-mov r0, #0
 strh r0, [r4,r1]
 
 Normal:

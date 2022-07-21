f89d7741554dfea607f82ac6328892f610ae0479
Retina1 <84415148+Retina1@users.noreply.github.com>
2022-07-20 23:14:58 -0400

    todd, dirk mugs, i swear i'll write more soon

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/MageSlayer/MageSlayer.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/MageSlayer/MageSlayer.s
@@ -1,48 +1,79 @@
 .thumb
-.equ MageSlayerID, SkillTester+4
-.equ gBattleData, 0x203A4D4
+.equ AuraSkillCheck, SkillTester+4
+.equ MageSlayerID, AuraSkillCheck+4
 
 push {r4-r7, lr}
 mov r4, r0 @atkr
 mov r5, r1 @dfdr
 
-
-@has mage slayer
+@has Loyalty
 ldr r0, SkillTester
 mov lr, r0
-mov r0, r4 @defender data
+mov r0, r4 @Attacker data
 ldr r1, MageSlayerID
 .short 0xf800
 cmp r0, #0
 beq End
 
-@make sure we're in combat (or battle forecast)
-ldrb r3, =gBattleData
-ldrb r3, [r3]
-cmp r3, #4
+@now check for the skill
+ldr r0, AuraSkillCheck
+mov lr, r0
+mov r0, r4 @attacker
+mov r1, #0x0
+mov r2, #0 @are allies
+mov r3, #2 @range
+.short 0xf800
+cmp r0, #0
 beq End
 
-@make sure the enemy is a mage
-ldr r0,[r5,#0x4]
-mov r1,#0x30
-ldr r0,[r0,r1] @so this loads the unit's staff/anima/light/dark prof
-cmp r0,#0x0
-beq End @if they're all 0 the enemy is not a mage
+Loop:
+ldrb r2,[r1]
+cmp r2,#0x0
+beq End
+add r1,#0x1
 
+mov r3,#0x48
+ldr r5,CharData
+sub r2,#0x1
+mul r3,r2
+add r5,r3
+mov r3,#0x4
+ldr r5,[r5,r3]
+ldrb r3,[r5,r3]
+cmp r3,#0x4a
+beq Final
+cmp r3,#0x2c
+beq Final
+b Loop
+
+
+Final:
 mov r1, #0x5a
-ldrh r0, [r4, r1] @Attack
-add r0, #2
-strh r0, [r4,r1]
+ldrh r2, [r4, r1]
+add r2, #0x3
+strh r2, [r4,r1]
 
-mov r1, #0x66
-ldrh r0, [r4, r1] @Crit
-add r0, #10
-strh r0, [r4,r1]
+mov r1, #0x5e
+ldrh r2, [r4, r1]
+add r2, #0x3
+strh r2, [r4,r1]
+
+mov r1, #0x60
+ldrh r2, [r4, r1]
+add r2, #15
+strh r2, [r4,r1]
+
+
 
 End:
 pop {r4-r7, r15}
 .align
 .ltorg
+CharData:
+.long 0x202be4c
+MovementPoin:
+.long 0x880bb96
 SkillTester:
 @Poin SkillTester
-@WORD MageSlayerID
+@WORD LoyaltyID
+

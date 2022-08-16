d914f75c9ec4dcf8d4371783b1f3d46692c459dd
Retina1 <84415148+Retina1@users.noreply.github.com>
2022-08-15 23:10:31 -0400

    ch 15 enemy placement skeleton, xanier

--- EngineHacks/Transform/TransformCommandUsability.asm
+++ EngineHacks/Transform/TransformCommandUsability.asm
@@ -18,22 +18,16 @@ CheckAdjacentAllies:
 ldr     r4,=#0x03004E50//unit in work ram
 ldr     r0,[r4]
 mov     r7,r0        //r7 has current unit
-add     r0,#0x10
-ldrb    r6,[r0]      //r6 has active unit X position
-ldrb    r5,[r0,#0x01]//r5 has active unit Y position
+ldrb    r6,[r0,#0x10]      //r6 has active unit X position
+ldrb    r5,[r0,#0x11]//r5 has active unit Y position
 ldr     r0,=#0x0202BE4C// charstruct
 //We iterate through each char and check if there's any adjacent allies
-CheckLoop:
 mov     r3,#0x00
-ldr     r1,[r0,r3]
-cmp     r1,#0x00
-beq     Disallow
-// if unit is null, check is over and no allies are adjacent
-add     r3,#0x10
-ldrb    r1,[r0,r3]      //r1 has unit X position
-add     r3,#0x01
-ldrb    r2,[r0,r3]//r2 has unit Y position
-sub     r3,#0x01
+CheckLoop:
+cmp     r3,#0x3D//61, max number of playables
+bhi     Disallow//if we exceed the playable structs count and find no targets, it's over
+ldrb    r1,[r0,#0x10]      //r1 has unit X position
+ldrb    r2,[r0,#0x11]//r2 has unit Y position
 cmp     r1,r6
 // we check for X -1, 0, +1 from relative position
 beq     CheckYEq
@@ -62,15 +56,30 @@ b       NextUnit
 
 AdjacentUnit:
 //if unit is adjacent, we check if unit is not themselves
-sub     r0,#0x10
 cmp     r0,r7
 //if unit checked = waiting unit, we don't end
 beq     NextUnit
+//we check if adjacent unit is dead, undeployed, or otherwise unavailable
+mov     r1,#0x95
+ldrb    r2,[r0,#0x0C]
+and     r2,r1
+cmp     r2,#0x00
+bne     NextUnit//if any of dead, not drawn or hidden by roof are true, we treat as unavailable
+//we check their deployment byte, just in case we try copying non-players
+mov     r2,#0xC0
+ldrb    r1,[r0,#0x0B]
+and     r1,r2
+cmp     r1,#0x00
+bne     NextUnit//if NPC or red unit bytes are true, we do not copy
+ldr     r1,[r0]
+cmp     r1,#0x00
+beq     NextUnit//if unit pointer is 0, struct is empty so there's no ally
 //Adjacent ally! we allow
 b       Allow
 
 NextUnit:
 add     r0,#0x48
+add     r3,#0x01
 b       CheckLoop
 
 
---
+++ Graphics/Animation/Freelancer(GabrielKnight)/README.md
@@ -0,0 +1,9 @@
+# [\[Custom Oracle\] \[F\] Oracle by GabrielKnight](https://github.com/Klokinator/FE-Repo/tree/main/Battle%20Animations/Magi%20-%20Special/%5BCustom%20Oracle%5D%20%5BF%5D%20Oracle%20by%20GabrielKnight) [![Download](https://img.shields.io/badge/Download--red?style=social&logo=github)](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Klokinator/FE-Repo/tree/main/Battle%20Animations/Magi%20-%20Special/%5BCustom%20Oracle%5D%20%5BF%5D%20Oracle%20by%20GabrielKnight)
+
+| <b>Magic</b><br/><img alt="Magic" src="https://raw.githubusercontent.com/Klokinator/FE-Repo/main/Battle%20Animations/Magi%20-%20Special/%5BCustom%20Oracle%5D%20%5BF%5D%20Oracle%20by%20GabrielKnight/6.%20Magic/Magic.gif"/> |
+| :---: |
+
+## Credits
+
+Made by {GabrielKnight}
+

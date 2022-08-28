6b177ccc2720fad237ae9bad022d66b21970d5a0
Retina1 <84415148+Retina1@users.noreply.github.com>
2022-08-27 23:09:19 -0400

    promotion based mugs, a bit more text

--- EngineHacks/ShopListExpansion/BuyItem.asm
+++
@@ -1,9 +0,0 @@
-//80B4690
-.thumb
-
-lsl   r2,#0x01
-ldr   r1,=#0x02026E30
-add   r1,r2
-ldrh  r1,[r1]
-ldr   r3,=#0x080B4699
-bx    r3
--- EngineHacks/ShopListExpansion/BuyItemConvoy.asm
+++
@@ -1,9 +0,0 @@
-//80B4704
-.thumb
-
-lsl   r1,#0x01
-ldr   r0,=#0x02026E30
-add   r0,r1
-ldrh  r0,[r0]
-ldr   r3,=#0x080B470D
-bx    r3

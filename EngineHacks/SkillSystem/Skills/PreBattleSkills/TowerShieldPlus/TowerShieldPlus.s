043025518f222187a50817d413b1870fff76d532
Retina1 <84415148+Retina1@users.noreply.github.com>
2022-09-06 22:34:59 -0400

    zeke gets supply skill in c15, a bit of clarity

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/TowerShieldPlus/TowerShieldPlus.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/TowerShieldPlus/TowerShieldPlus.s
@@ -10,37 +10,33 @@ push {r4-r7,lr}
 mov r4, r0
 mov r5, r1
 
-@check for skill
+@check for attacker w/ skill
 ldr r0, SkillTester
 mov lr, r0
 mov r0, r4
 ldr r1, TowerShieldPlusID
 .short 0xf800
 cmp r0, #0
+bne DoIt
+@check for defender w/ skill
+ldr r0, SkillTester
+mov lr, r0
+mov r0, r5
+ldr r1, TowerShieldPlusID
+.short 0xf800
+cmp r0, #0
 beq End
 
-@set both attacker and defender attack to 50 percent
+DoIt:
+@set both attacker and defender attack and defense to 50 percent
 mov r1, #0x5a
 ldrsh r0, [r4, r1] @atk
 lsr r0,r0,#0x1
 strh r0, [r4,r1]
-ldrsh r0, [r5, r1] @atk
+mov r1, #0x5c
+ldrsh r0, [r4, r1] @atk
 lsr r0,r0,#0x1
-strh r0, [r5,r1]
-
-mov  r2,#0x02
-ldrb r1, [r7]
-and  r1, r2
-cmp  r1, r2
-bne  End//if bit 2 is set, battle has NOT started
-mov  r2, #0x5c
-ldrh r3, [r5,r2]//load defender def
-mov  r2, #0x5A//atk offset
-ldrh r1, [r4,r2]//load attacker atk
-add  r1,r3
-strh r1, [r4,r2]
-b    End
-//endofcode
+strh r0, [r4,r1]
 
 End:
 pop {r4-r7}

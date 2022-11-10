599e123fc6b53ed5e509dc559c1e11ffb33cba99
Retina1 <84415148+Retina1@users.noreply.github.com>
2022-11-09 23:05:52 -0500

    real stealable icons finally, quixotic in ch2

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/OddRhythm/OddRhythm.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/OddRhythm/OddRhythm.s
@@ -5,32 +5,29 @@ push	{r4, lr}
 mov	r4, r0 @attacker
 mov	r5, r1 @defender
 
-@check if turn is odd
-ldr	r0,=#0x202BCF0
-ldrh	r0, [r0,#0x10]
-mov	r1, #0x01
-and	r0, r1
-cmp	r0, #0x00
-beq	End
-
-@has skill
+@either side has skill
 ldr	r0, SkillTester
 mov	lr, r0
 mov	r0, r4
 ldr	r1, OddRhythmID
 .short	0xf800
 cmp	r0, #0
+bne DoIt
+
+@has skill
+ldr	r0, SkillTester
+mov	lr, r0
+mov	r0, r5
+ldr	r1, OddRhythmID
+.short	0xf800
+cmp	r0, #0
 beq	End
 
-@add 10 to hit and avoid
+DoIt:
+@add 30 to hit for both sides
 mov	r0, #0x60
 ldrh	r1, [r4,r0]	@load hit
-add	r1, #0x0A	@add 20 to hit
-strh	r1, [r4,r0]     @store
-
-mov	r0, #0x62
-ldrh	r1, [r4,r0]	@load avoid
-add	r1, #0x0A	@add 20 to avoid
+add	r1, #0x1e	@add 30 to hit
 strh	r1, [r4,r0]     @store
 
 End:

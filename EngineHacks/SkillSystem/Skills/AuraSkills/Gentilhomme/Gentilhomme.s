7b8ecf17014e099e8600f96c35f35d2496dc3542
Retina1 <84415148+Retina1@users.noreply.github.com>
2022-12-16 23:06:04 -0500

    viridianing the first

--- EngineHacks/SkillSystem/Skills/AuraSkills/Gentilhomme/Gentilhomme.s
+++ EngineHacks/SkillSystem/Skills/AuraSkills/Gentilhomme/Gentilhomme.s
@@ -1,5 +1,5 @@
 @Spur Def: adjacent allies gain +4 defense in combat.
-.equ GentilhommeID, AuraSkillCheck+4
+.equ KillerTacticsID, AuraSkillCheck+4
 .thumb
 push {r4-r7,lr}
 @goes in the battle loop.
@@ -8,42 +8,45 @@ push {r4-r7,lr}
 mov r4, r0
 mov r5, r1
 
-CheckGender:
-@is the attacker male?
-ldr r0, [r4] @char
-ldr r0, [r0, #0x28] @char abilities
-ldr r1, [r4,#4] @class
-ldr r1, [r1,#0x28] @class abilities
-orr r0, r1
-mov r1, #0x40
-lsl r1, #8 @0x4000 IsFemale
-tst r0, r1
-beq Done @skip if male
+CheckHealth:
+ldrb r0, [r4, #0x12]
+lsr r0, #1 @max hp/2
+ldrb r1, [r4, #0x13] @currhp
+cmp r1, r0
+bgt Done
 
 CheckSkill:
 @now check for the skill
 ldr r0, AuraSkillCheck
 mov lr, r0
 mov r0, r4 @attacker
-ldr r1, GentilhommeID
+ldr r1, KillerTacticsID
 mov r2, #0 @can_trade
-mov r3, #2 @range
+mov r3, #1 @range
 .short 0xf800
 cmp r0, #0
 beq Done
 
-@ mov r0, r5
-@ add     r0,#0x5A    @Move to the defender's damage.
-@ ldrh    r3,[r0]     @Load the defender's damage into r3.
-@ sub     r3,#2    @Subtract 4 from the defender's damage.
-@ strh    r3,[r0]     @Store defender avoid.
+mov r1,#0x60
+ldrh r2,[r4,r1]
+sub r2,#0x1e
+strh r2,[r4,r1]
 
-@testing
+mov r0, r4       @Move attacker data into r1.
+mov r1, #0x4c    @Move to the attacker's weapon ability
+ldr r1, [r0,r1]
+mov r2, #0x20
+tst r1, r2
+bne     Done @do nothing if brave bit is set
+
 mov r0, r4
-add r0, #0x5c @attacker defense
+add     r0,#0x4c    @Move to the attacker's weapon bit
 ldrh r3, [r0]
-add r3, #2
+mov  r2, #0x20
+orr  r3, r2 @set the brave bit
 strh r3, [r0]
+
+b		Done
 
 Done:
 pop {r4-r7}
@@ -53,4 +56,4 @@ bx r0
 .ltorg
 AuraSkillCheck:
 @ POIN AuraSkillCheck
-@ WORD GentilhommeID
+@ WORD KillerTacticsID

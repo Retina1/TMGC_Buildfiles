17562e8ed64ee55107bc2cdaab17e7db2abfd068
Retina1 <84415148+Retina1@users.noreply.github.com>
2022-12-17 23:06:00 -0500

    chartreuse skill, viridian money setting

--- EngineHacks/SkillSystem/Skills/AuraSkills/UpWithArch/UpWithArch.s
+++ EngineHacks/SkillSystem/Skills/AuraSkills/UpWithArch/UpWithArch.s
@@ -1,87 +1,63 @@
-@Spur Def: adjacent allies gain +4 defense in combat.
-.equ UpWithArchID, AuraSkillCheck+4
+.equ ResonanceID, SkillTester+4
+.equ GetUnitsInRange, SkillTester+8
+.equ GetUnit,0x8019430|1
+.equ GetUnitMagic,0x80191B8|1
 .thumb
+.global Resonance
+.type Resonance, %function
+Resonance:
 push {r4-r7,lr}
 @goes in the battle loop.
 @r0 is the attacker
 @r1 is the defender
-mov	r4, r0
-mov	r5, r1
+mov    r4, r0
+mov    r5, r1
 
-CheckName:
-ldr	r0,[r4]
-ldrh	r0,[r0]
-ldr	r1,=#0x815D48C
-lsl	r0,#2
-add	r0,r1
-ldr	r0,[r0]
-ldrb	r1,[r0]
-cmp	r1,#0x41
-bne	Done
-ldrb	r1,[r0,#1]
-cmp	r1,#0x72
-bne	Done
-ldrb	r1,[r0,#2]
-cmp	r1,#0x63
-bne	Done
-ldrb	r1,[r0,#3]
-cmp	r1,#0x68
-bne	Done
-
-CheckSkill:
-@now check for the skill
-ldr	r0, AuraSkillCheck
-mov	lr, r0
-mov	r0, r4 @attacker
-ldr	r1, UpWithArchID
-mov	r2, #0 @can_trade
-mov	r3, #1 @range
-.short	0xf800
-mov	r5, #0x00
-mov	r6, r1
-mov	r7, #0x00
-cmp	r0, #0
-beq Done
+mov r0, r4 @Assuming unit struct
+mov r1, #0x0 @allyOption
+mov r2, #0x1 @Range
+ldr r3, GetUnitsInRange
+bl  BXR3
+cmp r0, #0x0
+beq End
+  mov r6, r0
+  mov r7, #0x0
 
 Loop:
-ldrb	r0, [r6,r5]
-cmp	r0, #0x00
-beq	StoreMight
-@testing
-ldr	r3,=#0x8019430	@gets character ram pointer
-mov	lr, r3
-.short	0xf800
-ldr	r3,=#0x80191B0	@gets character power
-mov	lr, r3
-.short	0xf800
-add	r7, r0		@add character power to total
-ldrb	r0, [r6,r5]
-ldr	r3,=#0x8019430	@gets character ram pointer
-mov	lr, r3
-.short	0xf800
-ldr	r3,=#0x8016B28	@gets character weapon id and uses
-mov	lr, r3
-.short	0xf800
-ldr	r3,=#0x80175DC	@gets weapon might from weapon short
-mov	lr, r3
-.short	0xf800
-add	r7, r0		@add to total
-add	r5, #0x01
-b	Loop
+ldrb r0, [r6, r7]
+cmp  r0, #0x0
+beq  End
+  ldr  r3, =GetUnit
+  bl   BXR3
+  ldr  r1, ResonanceID
+  ldr  r3, SkillTester
+  bl   BXR3
+  cmp  r0, #0x0
+  bne  AddBonus
+    add  r7, #0x1
+    b    Loop
 
-StoreMight:
-lsr	r7, #0x01	@divide total by 2
-add	r4, #0x5A
-ldrh	r0, [r4]	@load damage
-add	r0, r7		@add total to damage
-strh	r0, [r4]	@store damage
+AddBonus:
+ldrb r0, [r6, r7]
+ldr  r3, =GetUnit
+bl   BXR3
+ldr  r3, =GetUnitMagic
+bl   BXR3
+mov  r7, r0
+lsr  r7, #0x01    @divide total by 2
+add  r4, #0x5A
+ldrh r0, [r4]    @load damage
+add  r0, r7        @add total to damage
+strh r0, [r4]    @store damage
 
-Done:
+End:
 pop {r4-r7}
 pop {r0}
 bx r0
 .align
 .ltorg
-AuraSkillCheck:
-@ POIN AuraSkillCheck
-@ WORD UpWithArchID
+
+BXR3:
+bx r3
+.align
+SkillTester:

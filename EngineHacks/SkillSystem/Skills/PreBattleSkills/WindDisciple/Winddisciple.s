0aa6e168ff688bddf15b46d461d7c23818542ced
Retina1 <84415148+Retina1@users.noreply.github.com>
2023-03-24 23:18:47 -0400

    c21 work (playtest if you like very incomplete maps)

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/WindDisciple/Winddisciple.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/WindDisciple/Winddisciple.s
@@ -6,6 +6,15 @@ push {r4-r7, lr}
 mov r4, r0 @atkr
 mov r5, r1 @dfdr
 
+@defender has Winddisciple
+ldr r0, SkillTester
+mov lr, r0
+mov r0, r5 @attacker data
+ldr r1, WinddiscipleID
+.short 0xf800
+cmp r0, #0
+beq End
+
 @make sure we are in combat (or combat prep)
 ldr  r3, =gBattleData
 ldrb r3, [r3]
@@ -13,24 +22,15 @@ mov  r0, #0x3
 tst  r0, r3
 beq  End
 
-@attacker has Winddisciple
-ldr r0, SkillTester
-mov lr, r0
-mov r0, r4 @attacker data
-ldr r1, WinddiscipleID
-.short 0xf800
-cmp r0, #0
-beq End
-
 @lose a fuck ton of def (and crit avoid why not)
 mov r1, #0x5a
-ldrh r0, [r5, r1] @atk
+ldrh r0, [r4, r1] @atk
 add r0, #250
-strh r0, [r5,r1]
+strh r0, [r4,r1]
 mov r1, #0x66
-ldrh r0, [r5, r1] @crit
+ldrh r0, [r4, r1] @crit
 add r0, #250
-strh r0, [r5,r1]
+strh r0, [r4,r1]
 
 End:
 pop {r4-r7, r15}

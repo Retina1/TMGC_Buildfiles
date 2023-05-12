9f307dd6be6d93d87e950b9c9cd0b1b50071ccbe
Retina1 <84415148+Retina1@users.noreply.github.com>
2023-05-11 23:04:04 -0400

    interlude 9 work, some animations, instruments

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/LightWeight/LightWeight.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/LightWeight/LightWeight.s
@@ -1,15 +1,18 @@
 .thumb
-.equ LightWeightID, SkillTester+4
+.equ ItemTable, SkillTester+4
+.equ LightWeightID, ItemTable+4
+.equ GetItemWeight, 0x801760D
+.equ GetUnit,0x8019430
+.equ GetUnitCon, 0x8019284
+.macro blh to, reg
+    ldr \reg, =\to
+    mov lr, \reg
+    .short 0xF800
+.endm
 
 push {r4-r7, lr}
 mov r4, r0 @atkr
 mov r5, r1 @dfdr
-
-@Only has 3 or less items
-mov r1, #0x24
-ldrb r0, [r4, r1] @fourth item in inventory
-cmp r0, #0x0 		 @This item is empty
-bne End @skip if holding 4 items
 
 @has LightWieghtID
 ldr r0, SkillTester
@@ -20,10 +23,28 @@ ldr r1, LightWeightID
 cmp r0, #0
 beq End
 
-@add 3 AS
+mov r3,#0x4a
+ldrh r0,[r4,r3]
+blh GetItemWeight,r1
+mov r6,r0
+
+mov r3,#0xb
+ldrb r0,[r4,r3]
+blh GetUnit,r1 
+blh GetUnitCon,r1
+
+mov r2,r6
+
+sub r2,r0,r2
+cmp r2,#0
+ble End
+
+lsr r2,#1
+
+@add AS
 mov r1, #0x5E
 ldrh r0, [r4, r1] @AS
-add r0, #3
+add r0,r2
 strh r0, [r4,r1]
 
 End:

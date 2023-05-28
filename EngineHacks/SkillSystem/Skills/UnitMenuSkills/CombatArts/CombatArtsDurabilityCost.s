2b49208929981675362cd1069781c15036020ae1
Retina1 <84415148+Retina1@users.noreply.github.com>
2023-05-27 23:01:54 -0400

    oh boy more supports, knight aspirant buff

--- EngineHacks/SkillSystem/Skills/UnitMenuSkills/CombatArts/CombatArtsDurabilityCost.s
+++ EngineHacks/SkillSystem/Skills/UnitMenuSkills/CombatArts/CombatArtsDurabilityCost.s
@@ -26,6 +26,26 @@ ldr r0,=#0x203A4EC
 cmp r0,r4
 bne GoBack
 
+@has knight aspritant
+mov r0,r4
+ldr r1,=SkillTester
+mov r14,r1
+ldr r1,=KnightAspirantIDLink
+ldrb r1,[r1]
+.short 0xF800
+cmp r0,#1
+bne SkipCheck
+
+@Above 75% hp
+ldrb r0, [r4, #0x12]
+lsr r0, #2 @max hp/2
+mov r1,#0x3
+mul r0, r1
+ldrb r1, [r4, #0x13] @currhp
+cmp r1, r0
+bge GoBack
+
+SkipCheck:
 @table indexed by attack type containing extra durability cost
 ldr r0,=CombatArtCostTable
 ldr r1,=#0x0203F101 @location of attack type byte 

02332caf1603771c2ed9c815da636df02e12f49d
Retina1 <84415148+Retina1@users.noreply.github.com>
2023-06-26 22:40:57 -0400

    odium gauntlet, various textlings

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/Thighdeology/Thighdeology.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/Thighdeology/Thighdeology.s
@@ -1,47 +1,39 @@
 .thumb
 .equ ThighdeologyID, SkillTester+4
-.equ AuraSkillCheck, ThighdeologyID+4
-.equ NiceThighsID, AuraSkillCheck+4
 
 push {r4-r7, lr}
 mov r4, r0 @attacker
 mov r5, r1 @defendker
 
+@has Thighdeology
 ldr r0, SkillTester
 mov lr, r0
-mov r0, r4 @attacker data
+mov r0, r4 @Attacker data
 ldr r1, ThighdeologyID
 .short 0xf800
 cmp r0, #0
-beq GoBack
+beq End
 
-@if we have Thighdeology, check for people with Nice Thighs
-ldr r0, AuraSkillCheck
-mov lr, r0
-mov r0, r4 @attacker
-ldr r1, NiceThighsID
-mov r2, #4 @all units
-mov r3, #3 @range
-.short 0xf800
-cmp r0, #0
-beq GoBack
+@check sword
+mov		r1,#0x50
+ldrb r0,[r4,r1] @weapon type
+cmp r0,#0x2
+bne End
 
-@+2 Attack and +20 Hit
+@get crit in r6
+mov		r1,#0x66
+ldrh	r6,[r4,r1]
+lsr		r6,#2
+mov r0,#0
+strh	r0,[r4,r1]
 
-mov r0,r4
-add r0,#0x5A
-ldrh r1,[r0]
-add r1,#2
-strh r1,[r0]
+@do the thing
+mov r1, #0x5a
+ldrh r0, [r4, r1]
+add r0, r6
+strh r0, [r4,r1]
 
-mov r0,r4
-add r0,#0x60
-ldrh r1,[r0]
-add r1,#20
-strh r1,[r0]
-
-
-GoBack:
+End:
 pop {r4-r7, r15}
 .align
 .ltorg

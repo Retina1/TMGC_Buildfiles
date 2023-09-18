586ec5e83fa40ccf1ca077e174f4b9adbf511793
Retina1 <84415148+Retina1@users.noreply.github.com>
2023-09-17 23:37:22 -0400

    ch25 main story writing mostly

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/Fortune/Fortune.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/Fortune/Fortune.s
@@ -10,42 +10,26 @@ push {r4-r7,lr}
 mov r4, r0
 mov r5, r1
 
-ldr	 r0,=#0x203A4D4	@pre-battle data pointer, gonna check if a target has been selected or the fight has started (0x02 if targeting someone, 0x01 if battle started)
-ldrb r0,[r0]
-mov  r1,#0x3
-tst  r0,r1
-beq	 GoBack
-
+@has ArcaneBlade
 ldr r0, SkillTester
 mov lr, r0
-mov r0, r4
+mov r0, r4 @Attacker data
 ldr r1, FortuneID
 .short 0xf800
 cmp r0, #0
-beq CheckDefender
+beq End
 
-@crit
-mov r0,r5
-add r0,#0x66 
-mov r3,#0
-strh r3,[r0]
+@store magic in r6
+mov		r1,#0x3A
+ldrb	r6,[r4,r1]
 
-CheckDefender:
-ldr r0, SkillTester
-mov lr, r0
-mov r0, r5
-ldr r1, FortuneID
-.short 0xf800
-cmp r0,#0
-beq GoBack
+@add magic to attack
+mov r1, #0x5a
+ldrh r0, [r4, r1]
+add r0, r6
+strh r0, [r4,r1]
 
-@crit
-mov r0,r4
-add r0,#0x66
-mov r3,#0
-strh r3,[r0]
-
-GoBack:
+End:
 pop {r4-r7}
 pop {r0}
 bx r0

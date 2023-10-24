12931d7c23b956cd79a0682f9a1f2d9b31ff541f
Retina1 <84415148+Retina1@users.noreply.github.com>
2023-10-23 22:51:25 -0400

    support anim toggle, various changes

--- EngineHacks/QualityOfLife/HpBars/GameOptions/ForceBattleOffWithDanceAndStaff.s
+++ EngineHacks/QualityOfLife/HpBars/GameOptions/ForceBattleOffWithDanceAndStaff.s
@@ -13,20 +13,15 @@ cmp r4, #0x0
 beq Exit			@アニメOFFなので何もしない
 
 @杖と踊りアニメの設定の確認
-@ldr r0, =0x0202BCEC  @ChapterData	{J}
-ldr r0, =0x0202BCF0  @ChapterData	{U}
-add r0, #0x41
-ldrb r0, [r0]
-                   @0x02 効果音 OFF
-                   @0x80 字幕ヘルプ OFF
-mov r1, #0x82
-and r0, r1
-cmp r0, #0x0
+ldr r0,=#0x8083da8 @CheckEventId
+mov r14,r0
+mov r0,#0xae @support anim flag
+.short 0xF800
+cmp r0,#0
 beq Exit           @杖と踊りアニメはオンなので何もしない
 
 
 @自分はボスですか?
-@ldr	 r3, =0x0203A4E8	@gBattleActor	@{J}
 ldr	 r3, =0x0203A4EC	@gBattleActor	@{U}
 ldr	 r0, [r3, #0]
 ldrh	r0, [r0, #0x28]
@@ -37,13 +32,13 @@ bne		Exit			@自分はボスなのでアニメを表示します
 @杖と踊りならアニメオフにする
 mov  r2, #0x50		@WeaponType
 ldrb r2, [r3, r2]	@gBattleActor->WeaponType
-cmp  r2, #0xFF		@Dance
-beq  AnimeOFF
 cmp  r2, #0x04		@Staff
-beq  DanceCheck
+beq  CanCounterCheck
+cmp  r2, #0xFF		@Dance
+beq  CanCounterCheck
 b    Exit
 
-DanceCheck:
+CanCounterCheck:
 mov  r2, #0x52		@canCounter
 ldrb r2, [r3, r2]	@gBattleActor->canCounter
 cmp  r2, #0x0       @Danceは常に1になる
@@ -55,13 +50,10 @@ mov  r4, #0x0
 
 Exit:
 @壊すコードの再送
-@blh 0x080599a0	@{J}
 blh 0x08058b70	@{U}
-@ldr r0, =0x0203E11C @gSomethingRelatedToAnimAndDistance	@{J}
 ldr r0, =0x0203E120 @gSomethingRelatedToAnimAndDistance	@{U}
 mov r1, #0x0
 ldsh r0, [r0, r1]
 
-@ldr r3, =0x08058A08|1	@{J}
 ldr r3, =0x08057AC0|1	@{U}
 bx  r3
---
+++ EngineHacks/QualityOfLife/HpBars/GameOptions/GenericToggle.s
@@ -0,0 +1,55 @@
+.align 4
+.macro blh to, reg=r3
+  ldr \reg, =\to
+  mov lr, \reg
+  .short 0xf800
+.endm
+.thumb
+
+	.equ CheckEventId,0x8083da8
+	.equ SetEventId, 0x8083d80 
+	.equ UnsetEventId, 0x8083d94
+	.equ GameOptionHandler, 0x80B1D14
+	
+
+
+	
+.type GenericToggleFunc, %function
+.global GenericToggleFunc
+
+GenericToggleFunc:
+push {r4, lr}
+mov r4, r0 @ ID 
+@ vanilla just pushed lr 
+mov r0, r4 
+blh CheckEventId
+cmp r0, #0 
+beq SetFlagOn
+mov r0, r4
+blh UnsetEventId
+b End 
+SetFlagOn:
+mov r0, r4
+blh SetEventId 
+End:
+pop {r4}
+pop {r1}
+pop {r1}
+bx r1 
+
+.ltorg 
+.align 
+
+.type GenericCheckOption, %function 
+.global GenericCheckOption
+@ pop lr twice 
+GenericCheckOption:
+push {r4, lr}
+blh CheckEventId
+pop {r4}
+pop {r1}
+pop {r1}
+bx r1 
+
+.ltorg 
+.align 

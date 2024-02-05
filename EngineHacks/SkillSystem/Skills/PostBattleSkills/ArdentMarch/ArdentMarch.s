b76a52706062881e4d51910133227c08dd998e36
Retina1 <84415148+Retina1@users.noreply.github.com>
2024-02-04 23:13:27 -0500

    most 26 writing done, ardent march

---
+++ EngineHacks/SkillSystem/Skills/PostBattleSkills/ArdentMarch/ArdentMarch.s
@@ -0,0 +1,63 @@
+.macro blh to, reg=r3
+  ldr \reg, =\to
+  mov lr, \reg
+  .short 0xf800
+.endm
+.equ RoboKyID, SkillTester+4
+.equ HeatEvent, RoboKyID+4
+.thumb
+push	{lr}
+
+@check if attacked this turn
+ldrb 	r0, [r6,#0x11]	@action taken this turn
+cmp	r0, #0x2 @attack
+bne	End
+
+CheckDefender:
+@check for skill
+mov r0, r5
+ldr r1, RoboKyID
+ldr r3, SkillTester
+mov lr, r3
+.short  0xf800
+cmp r0,#0x00
+bne Event
+
+@check if dead
+ldrb	r0, [r4,#0x13]
+cmp	r0, #0x00
+beq	End
+
+@check for skill
+mov	r0, r4
+ldr	r1, RoboKyID
+ldr	r3, SkillTester
+mov	lr, r3
+.short	0xf800
+cmp	r0, #0x00
+beq	End
+@check for flag
+ldr r0,=#0x8083da8 @CheckEventId
+mov r14,r0
+mov r0,#0xa2 @tension flag
+.short 0xF800
+cmp r0,#0
+bne	End
+
+@deactivate flag
+Event:
+ldr	r0,=#0x800D07C		@event engine thingy
+mov	lr, r0
+ldr	r0, HeatEvent	@this event gives heat
+mov	r1, #0x01		@0x01 = wait for events
+.short	0xF800
+
+End:
+pop	{r0}
+bx	r0
+.ltorg
+.align
+SkillTester:
+@POIN SkillTester
+@WORD RoboKyID
+@POIN DespoilEvent

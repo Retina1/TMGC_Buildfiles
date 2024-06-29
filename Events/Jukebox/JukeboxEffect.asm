8d61080ae51fa32c810723ec2b173a28f5bf7572
Retina1 <84415148+Retina1@users.noreply.github.com>
2024-06-28 23:01:48 -0400

    jukebox, interlude light runes

---
+++ Events/Jukebox/JukeboxEffect.asm
@@ -0,0 +1,12 @@
+.thumb
+push {lr}
+ldr r0,=#0x800D07C    @event engine thingy
+mov lr, r0
+ldr r0, AimEvent @this event is just "play sound"
+mov r1, #0x01   @0x01 = wait for events
+.short  0xF800
+mov r0,#0x17
+pop {r1} 
+bx r1
+.ltorg
+AimEvent:
---
+++ Events/Jukebox/JukeboxUsability.asm
@@ -0,0 +1,21 @@
+.thumb
+.org 0
+push {r4-r7,lr}
+
+ldr r0,=#0x8083da8 @CheckEventId
+mov r14,r0
+mov r0,#0x90 @luck flag
+.short 0xF800
+cmp r0,#0
+beq False
+
+True:
+mov r0,#1
+b End
+
+False:
+mov r0,#3
+End:
+pop {r4-r7}
+pop {r1}
+bx r1

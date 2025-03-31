58c9da13903f7e1a1f0920779671d77522f344e6
Retina <84415148+Retina1@users.noreply.github.com>
2025-03-30 22:26:27 -0400

    pile of mugs and cgs

---
+++ EngineHacks/HelpText/Modules/Skills/asmExamples/TurnSevenCheck.asm
@@ -0,0 +1,27 @@
+.thumb
+
+.include "../SkillsDefs.s"
+
+.global TurnSevenCheck
+.type TurnSevenCheck, %function
+
+
+		TurnSevenCheck:
+
+		ldr	r0,=#0x202BCF0
+		mov		r3, #0
+		ldrh	r0, [r0,#0x10]
+		mov r1, #0x7
+		swi 6
+		cmp	r1, #0x0
+		bne	End
+		
+			mov		r3, #1
+				
+		End:
+		mov		r0, r3
+		bx		r14
+		
+		.align
+		.ltorg
+
